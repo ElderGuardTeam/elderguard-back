8885bd86cb5e78d69716e312f21989c8bad05e43
Geovanni Magnani <magnanigeovanni@gmail.com>
2025-04-06 10:32:38 -0300

    feat: add question and option but without configurate

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,11 @@ import { UserModule } from './user/user.module';
 import { ProfessionalModule } from './professional/professional.module';
 import { AuthModule } from './auth/auth.module';
 import { MailModule } from './mail/mail.module';
+import { QuestionModule } from './question/question.module';
+import { OptionModule } from './option/option.module';
 
 @Module({
-  imports: [DatabaseModule, ElderlyModule, ContactModule, AddressModule, UserModule, ProfessionalModule, AuthModule, MailModule],
+  imports: [DatabaseModule, ElderlyModule, ContactModule, AddressModule, UserModule, ProfessionalModule, AuthModule, MailModule, QuestionModule, OptionModule],
   controllers: [AppController],
   providers: [AppService],
 })
