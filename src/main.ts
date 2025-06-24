9e6a6dc14817b70f44ae52dfb14d2e0f4c4d5175
BrunaBudel <bruna.budel@houden.com.br>
2025-06-24 17:26:21 -0300

    chore: image upload

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { join } from 'path'; // Importe o 'join' do 'path'
+import { join } from 'path';
+import * as express from 'express'; // 👈 adicionado
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // Aumenta o limite de body
+  app.use(express.json({ limit: '10mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
   app.enableCors();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads/',
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
